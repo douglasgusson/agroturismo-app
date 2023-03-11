b51bcb0dd0e4ec497583b9a688f4e97455935929
Douglas Gusson <douglasgusson@gmail.com>
2023-03-11 12:05:47 -0300

    Inicio layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,134 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const categories = [
+  {
+    name: "New Arrivals",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
+  },
+  {
+    name: "Productivity",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg",
+  },
+  {
+    name: "Workspace",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg",
+  },
+  {
+    name: "Accessories",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg",
+  },
+  {
+    name: "Sale",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg",
+  },
+];
 
 export default function Home() {
   return (
-    <main className={inter.className}>
-     <div className="w-full h-screen">
-      Hello!
+    <main className={`lg:relative ${inter.className}`}>
+      <div className="mx-auto flex w-full max-w-7xl flex-wrap items-center pt-16 pb-20 text-center lg:py-48 lg:text-left">
+        <div className="px-4 sm:px-8 lg:w-1/2 xl:pr-16">
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+            <span className="block xl:inline">Agroturismo</span>{" "}
+            <span className="block text-indigo-600 xl:inline">Capixaba</span>
+          </h1>
+          <p className="mx-auto mt-3 max-w-md text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <a
+                href="#"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+              >
+                Explorar
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <Image
+          src="/img/nature.svg"
+          alt="Desenho de natureza"
+          width={500}
+          height={500}
+        />
       </div>
+
+      <section
+        aria-labelledby="category-heading"
+        className="pt-24 sm:pt-32 xl:mx-auto xl:max-w-7xl xl:px-8"
+      >
+        <div className="px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8 xl:px-0">
+          <h2
+            id="category-heading"
+            className="text-2xl font-extrabold tracking-tight text-gray-900"
+          >
+            Shop by Category
+          </h2>
+          <a
+            href="#"
+            className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block"
+          >
+            Browse all categories<span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+
+        <div className="mt-4 flow-root">
+          <div className="-my-2">
+            <div className="relative box-content h-80 overflow-x-auto py-2 xl:overflow-visible">
+              <div className="min-w-screen-xl absolute flex space-x-8 px-4 sm:px-6 lg:px-8 xl:relative xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-x-0 xl:px-0">
+                {categories.map((category) => (
+                  <a
+                    key={category.name}
+                    href={category.href}
+                    className="relative flex h-80 w-56 flex-col overflow-hidden rounded-lg p-6 hover:opacity-75 xl:w-auto"
+                  >
+                    <span aria-hidden="true" className="absolute inset-0">
+                      <img
+                        src={category.imageSrc}
+                        alt=""
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"
+                    />
+                    <span className="relative mt-auto text-center text-xl font-bold text-white">
+                      {category.name}
+                    </span>
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 px-4 sm:hidden">
+          <a
+            href="#"
+            className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
+          >
+            Browse all categories<span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
