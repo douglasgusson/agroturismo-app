db1274fbb7b15956bef82f112b1903b75b553e5d
Douglas Gusson <douglasgusson@gmail.com>
2023-05-02 18:06:06 -0300

    :sparkles: Adição link página de roteiro

--- src/app/itinerary/page.tsx
+++ src/app/itinerary/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
         <ul role="list" className="steps steps-vertical px-4">
           {locals.map((local) => (
             <li key={local.name} className="step text-left">
-              <a href="#" className="block">
+              <Link href={`/local/${local.slug}`} className="block">
                 <div className="px-4 py-4 sm:px-6">
                   <div className="flex items-center justify-between">
                     <p className="truncate text-lg font-semibold">
@@ -46,21 +46,21 @@ export default function Page() {
                     </div>
                   </div>
                 </div>
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
 
-        <div className="mt-4">
+        <div className="mt-4 flex justify-center">
           <Link
             href={mapsRouteUrl}
             target="_blank"
             role="button"
             rel="noopener noreferrer"
-            className="btn-primary btn-wide btn gap-2"
+            className="inline-flex items-center rounded-full border border-transparent bg-slate-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2"
           >
             <MapIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-            Ver no Google Maps
+            Ver itinerário no mapa
           </Link>
         </div>
       </div>
