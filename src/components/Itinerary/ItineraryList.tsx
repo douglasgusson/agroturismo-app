ded76ce77d4da45c3d76322b8725b7ce27900327
Douglas Gusson <douglasgusson@gmail.com>
2023-05-06 17:38:43 -0300

    Ajuste layout lista locais roteiro

--- src/components/Itinerary/ItineraryList.tsx
+++ src/components/Itinerary/ItineraryList.tsx
@@ -12,13 +12,13 @@ export const ItineraryList: React.FC<ItineraryListProps> = ({
   onRemove,
 }) => {
   return (
-    <ul role="list" className="steps steps-vertical px-4">
+    <ul role="list" className="px-4">
       {locals.map((local) => (
-        <li key={local.name} className="step text-left">
+        <li key={local.name} className="text-left">
           <div className="flex items-center justify-between">
             <Link href={`/local/${local.slug}`} className="block">
               <div className="px-4 py-4 sm:px-6">
-                <div className="flex items-center justify-between">
+                <div className="flex flex-wrap items-center justify-between">
                   <p className="truncate text-lg font-semibold">{local.name}</p>
                   <div className="ml-2 flex flex-shrink-0">
                     <p className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
@@ -28,7 +28,7 @@ export const ItineraryList: React.FC<ItineraryListProps> = ({
                 </div>
                 <div className="mt-2">
                   <div className="flex">
-                    <p className="flex items-center text-left text-sm text-gray-500">
+                    <p className="flex items-center text-left text-xs text-gray-500 md:text-sm">
                       <MapPinIcon
                         className="mr-2 h-5 w-5 flex-shrink-0 text-gray-400"
                         aria-hidden="true"
@@ -39,29 +39,28 @@ export const ItineraryList: React.FC<ItineraryListProps> = ({
                 </div>
               </div>
             </Link>
-            <div className="flex-shrink-0 pr-2">
-              <button
-                onClick={() => onRemove(local)}
-                title="Remover do roteiro"
-                className="inline-flex items-center justify-center rounded-full border border-transparent bg-rose-500 px-2 py-2 text-sm font-medium text-white shadow-sm hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:ring-offset-2"
+
+            <button
+              onClick={() => onRemove(local)}
+              title="Remover do roteiro"
+              className="flex items-center justify-center rounded-full border border-transparent bg-rose-500 p-1 text-sm font-medium text-white shadow-sm hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:ring-offset-2"
+            >
+              <span className="sr-only">Remover do roteiro</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
               >
-                <span className="sr-only">Remover do roteiro</span>
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
           </div>
         </li>
       ))}
