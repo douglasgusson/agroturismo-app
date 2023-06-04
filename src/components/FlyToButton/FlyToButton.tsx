08550c4e940431f0f769e2aa1c9626da7fe91e1e
Douglas Gusson <douglasgusson@gmail.com>
2023-06-04 15:34:27 -0300

    Correção ícone localização usuário

--- src/components/FlyToButton/FlyToButton.tsx
+++ src/components/FlyToButton/FlyToButton.tsx
@@ -1,3 +1,4 @@
+import { LocateFixed } from "lucide-react";
 import { useMap } from "react-leaflet";
 
 export type FlyToButtonProps = {
@@ -16,19 +17,8 @@ export const FlyToButton = ({ coords }: FlyToButtonProps) => {
       className="absolute right-4 top-4 z-[1001] rounded-full bg-white p-3 shadow-md"
       onClick={handleClick}
     >
-      <svg
-        className="h-6 w-6 text-gray-600"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2.5"
-          d="M19 14l-7 7m0 0l-7-7m7 7V3"
-        />
-      </svg>
+      <LocateFixed className="h-6 w-6 text-gray-600" />
+      <span className="sr-only">Ir para a localização atual do usuário</span>
     </button>
   );
 };
